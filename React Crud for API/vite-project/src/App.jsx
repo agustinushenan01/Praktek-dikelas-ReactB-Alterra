31f7dc0f85f97d5fc81f594edd33601131e6d476
agustinushenan01 <agustinushenan01@gmail.com>
2024-04-19 16:36:23 +0800

    Update Styling

--- React Crud for API/vite-project/src/App.jsx
+++ React Crud for API/vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './index.css';
+import './Loading.css';
 
 function App() {
   const [data, setData] = useState([]);
@@ -9,6 +10,7 @@ function App() {
   const [editName, setEditName] = useState('');
   const [editAge, setEditAge] = useState('');
   const FecthURL = "https://660fae8d356b87a55c52086e.mockapi.io/Todo";
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +20,8 @@ function App() {
       } catch (error) {
         console.error('Failed to fetch data:', error);
         // Handle error appropriately
+      } finally {
+        setIsLoading(false); // Set isLoading to false after fetching data
       }
     };
 
@@ -30,7 +34,7 @@ function App() {
   }
 
   function validateAge(age) {
-    const regex =  /^[0-9]+$/;
+    const regex = /^[0-9]+$/;
     return regex.test(age);
   }
 
@@ -56,7 +60,7 @@ function App() {
   const handleDelete = async (id) => {
     try {
       await axios.delete(`${FecthURL}/${id}`);
-      const newData = data.filter(item => item.id !== id);
+      const newData = data.filter(data => data.id !== id);
       setData(newData);
       alert("Successfully deleted data")
     } catch (error) {
@@ -78,11 +82,11 @@ function App() {
       };
       await axios.put(`${FecthURL}/${id}`, TodoDataEdit);
       // Setelah berhasil diubah, update data secara lokal
-      const updatedData = data.map(item => {
-        if (item.id === id) {
-          return { ...item, Name: editName, Age: editAge };
+      const updatedData = data.map(data => {
+        if (data.id === id) {
+          return { ...data, Name: editName, Age: editAge };
         }
-        return item;
+        return data;
       });
       setData(updatedData);
       // Reset form edit
@@ -99,64 +103,37 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col mx-3 my-4 bg-violet-100 shadow-lg rounded-lg w-72'>
-        <form onSubmit={handleSubmit} action="post" className='flex flex-col px-2 py-2'>
-          <h2 className='text-center mb-4 font-medium text-2xl'>Tambah Data</h2>
-          <input type="text" value={name} onChange={(e) => {
-            const value = e.target.value;
-            if (validateName(value) || value === '') {
-              setName(value);
-            } else if( value === '' || value.length === 0 ) {
-              alert('Invalid name format.');
-            } else {
-              alert('Invalid name format.');
-            }
-          }} placeholder='Input your name' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
-          <input type="number" value={age} onChange={(e) => {
-            const value = e.target.value;
-            if (validateAge(value)) {
-              setAge(value);
-            } else if( value === '' || value.length === 0 ) {
-              alert('Invalid age format.');
-            } else {
-              alert('Invalid age format.');
-            }
-          }} placeholder='Input your age' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
-          <button type="submit" className='bg-cyan-500 hover:bg-cyan-600 rounded-lg text-white text-lg'>Submit</button>
-        </form>
-      </div>
-      <h1 className='text-3xl font-medium px-3'>Fecth data</h1>
+      <h1 className='text-3xl font-medium px-3'>Fecth Data with Axios</h1>
+      {isLoading ? ( // Tampilkan pesan loading jika isLoading true
+        <div className="flex justify-center items-center h-screen">
+        <div className="text-center my-4 flex mx-auto">
+            <p className='mr-1'>Loading</p>
+            <div className='spinner'></div>
+        </div>
+    </div>
+      ) : (
+        <div className='flex flex-col mx-3 my-4 bg-violet-100 shadow-lg rounded-lg w-72'>
+          <form onSubmit={handleSubmit} action="post" className='flex flex-col px-2 py-2'>
+            <h2 className='text-center mb-4 font-medium text-2xl'>Tambah Data</h2>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Input your name' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
+            <input type="number" value={age} onChange={(e) => setAge(e.target.value)} placeholder='Input your age' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
+            <button type="submit" className='bg-cyan-500 hover:bg-cyan-600 rounded-lg text-white text-lg'>Submit</button>
+          </form>
+        </div>
+      )}
       {data.map((Todo) => (
-        <div className='px-3' key={Todo.id}>
-          <img className='w-32 h-32' src={Todo.Avatar} alt="Avatar" />
+        <div className='px-3 bg-sky-50 py-2 shadow-lg mb-6 rounded-lg' key={Todo.id}>
+          <img className='w-32 h-32 shadow-lg rounded-lg' src={Todo.Avatar} alt="Avatar" />
           <p><span className='font-medium'>Name:</span> {capitalizeFirstLetter(Todo.Name)}</p>
           <p><span className='font-medium'>Age:</span> {capitalizeFirstLetter(Todo.Age)}</p>
           <p><span className='font-medium'>Comment:</span> {capitalizeFirstLetter(Todo.Comment)}</p>
           <button onClick={() => handleDelete(Todo.id)} className='bg-red-500 hover:bg-red-600 rounded-lg px-4 text-white text-lg mb-3'>Delete</button>
 
           {/* Fungsi Edit Data */}
-          <div className='flex flex-col px-2 py-2 w-36 bg-blue-50 rounded-lg shadow-lg'>
-            <h2 className='text-center mb-4 font-medium text-2xl'>Edit Data</h2>
-            <input type="text" value={editName} onChange={(e) => {
-              const value = e.target.value;
-              if (validateName(value) || value === '') {
-                setEditName(value);
-              } else if( value === '' || value.length === 0 ) {
-                alert('Invalid name format.');
-              } else {
-                alert('Invalid name format.');
-              }
-            }} placeholder='Edit your name' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
-            <input type="number" value={editAge} onChange={(e) => {
-              const value = e.target.value;
-              if (validateAge(value) || value === '') {
-                setEditAge(value);
-              } else if( value === '' || value.length === 0 ) {
-                alert('Invalid age format.');
-              } else {
-                alert('Invalid age format.');
-              }
-            }} placeholder='Edit your age' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
+          <div className='flex flex-col px-2 py-2 w-36 bg-indigo-600 rounded-lg shadow-lg'>
+            <h2 className='text-center mb-4 font-medium text-2xl text-white'>Edit Data</h2>
+            <input type="text" value={editName} onChange={(e) => setEditName(e.target.value)} placeholder='Edit your name' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
+            <input type="number" value={editAge} onChange={(e) => setEditAge(e.target.value)} placeholder='Edit your age' className='border px-1 border-emerald-400 rounded-lg focus:ring focus:outline-none focus:ring-cyan-400 mb-2' />
             <button onClick={() => handleEdit(Todo.id)} className='bg-cyan-500 hover:bg-cyan-600 rounded-lg text-white text-lg'>Save</button>
           </div>
           <br />
